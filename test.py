2aa45934c1999b809637e6e9bec023bf9e79ebf4
LuigiSobrenome <44helpap@gmail.com>
2023-02-14 14:18:32 +0000

    adding sentry with an error

--- test.py
+++ test.py
@@ -1,1 +1,15 @@
-#no code
+import sentry_sdk
+
+sentry_sdk.init(
+    dsn="https://deb7a6a91a904acfb63bc877d67482c0@o388665.ingest.sentry.io/5225844",
+    release="nocode",
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+)
+
+try:
+	raise NameError('Suspect commit test')
+except Exception as e:
+	sentry_sdk.capture_exception(e) 
