d2b79fdcabea38fdb4a00a1a2b65bbff1ea3658b
LuigiSobrenome <44helpap@gmail.com>
2023-04-26 15:44:40 +0000

    fixing bug, adding sentry

--- api/app.py
+++ api/app.py
@@ -3,6 +3,23 @@ from flask import (
     request,
 )
 
+import sentry_sdk
+
+sentry_sdk.init(
+    dsn="https://deb7a6a91a904acfb63bc877d67482c0@o388665.ingest.sentry.io/5225844",
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # By default the SDK will try to use the SENTRY_RELEASE
+    # environment variable, or infer a git commit
+    # SHA as release, however you may want to set
+    # something more human-readable.
+    # release="myapp@1.0.0",
+)
+
 from calculator.calculator import Calculator
 
 app = Flask(__name__)
@@ -31,5 +48,9 @@ def operation(method, num_factors):
 
     return str(getattr(Calculator, method)(*factors))
 
+@app.route('/error')
+    def ohno():
+        division_by_zero = 1/0
+        return
 
 app.run(host='0.0.0.0', port=8080)
